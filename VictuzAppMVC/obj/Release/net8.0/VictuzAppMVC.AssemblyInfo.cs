a217db9ad00ae0d95de7463fbf6a6da50dd60ae1
Soulaiman <svahawky@gmail.com>
2024-11-07 20:12:11 +0100

    packages installed

--- VictuzAppMVC/obj/Release/net8.0/VictuzAppMVC.AssemblyInfo.cs
+++ VictuzAppMVC/obj/Release/net8.0/VictuzAppMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VictuzAppMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579668dcf9ab30bb20e1f4c1a54c6505bb70adcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("VictuzAppMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VictuzAppMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
