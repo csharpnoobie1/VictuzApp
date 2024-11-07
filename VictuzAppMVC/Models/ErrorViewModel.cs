579668dcf9ab30bb20e1f4c1a54c6505bb70adcb
Soulaiman <svahawky@gmail.com>
2024-11-07 19:30:52 +0100

    MVC aangemaakt

---
+++ VictuzAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VictuzAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
